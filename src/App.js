efa629f5c275f7a804e000339b6878aac8efe917
haeyonghahn <yong80211@gmail.com>
2021-11-07 01:59:04 +0900

    react-router 사용

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 import React from 'react';
-import axios from 'axios';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import backGround from '../public/assets/backGround.mp4';
-import './App.scss'
+import Main from './components/Main';
+import Login from './components/Login';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      message: null
-    }
   }
   render() {
     return (
+      <BrowserRouter>
       <div id="App">
-        <video muted="muted" preload="auto" autoPlay="autoplay" loop="loop" volume="0">
-          <source src={backGround} type="video/mp4"/>
-        </video>
-        <div id="login-section">
-          <div id="login-info"> 
-            <h2 id="login-title">Welcome to Travler</h2>
-            <h4 id="login-title-description">More Fun, More Interested</h4>
-            <p>Include photos taken while traveling around the world.<br>
-            </br>Record your memories.</p>
-          </div>
-          <button type="button" className="btn btn-light" id="btnJoin">Join</button>
-        </div>
+          <Switch>
+            <Route exact path="/" render={() => <Main /> }></Route>
+            <Route path="/login" render={() => <Login /> }></Route>
+            <Redirect path="*" to="/" />
+          </Switch>
       </div>
+      </BrowserRouter>
     );
-  }
-  componentDidMount() {
-    axios.get('/api/hello', { 
-      // params: { id: 1 } 
-    })
-    .then(function(response) {
-      this.setState({
-        message: response.data
-      })
-    }.bind(this))
-    .catch(function(error) { 
-      console.log(error);
-    });
   }
 }
---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="Login">
+        <h2>Login 페이지</h2>
+      </div>
+    );
+  }
+}
