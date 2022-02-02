7e6413766c4feabfdd8eb58b7737e80e07f0f80c
haeyonghahn <yong80211@gmail.com>
2022-02-02 18:57:44 +0900

    로그인 ui 수정

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,88 +1,71 @@
 import React from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { TextField } from '@mui/material';
+import { Checkbox } from '@mui/material';
+import { Button } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
+import { Link } from '@mui/material';
+import { Grid } from '@mui/material';
+import { Typography } from '@mui/material';
+// import { LockOutlinedIcon } from '@mui/icons-material';
+import { Avatar } from '@mui/material';
+import { Box } from '@mui/material';
+import { Container } from '@mui/material';
 
-import AuthenticationService from '../api/AuthenticationService.js';
-import LoginImage from '../../public/assets/login.jpg';
-import './Login.scss';
-
-export default class Login extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: localStorage.getItem("authenticatedUser") || '',
-      password: "",
-      toekn: localStorage.getItem("token") || '',
-      hasLoginFailed: false,
-      showSuccessMessage: false
-    }
-    this.inputHandler = this.inputHandler.bind(this);
-    this.loginHandler = this.loginHandler.bind(this);
-  }
-
-  inputHandler(e) {
-    console.log(e.target.name + " : " + e.target.value);
-    this.setState({
-      [e.target.name] : e.target.value
-    });
-  }
-
-  loginHandler() {
-    AuthenticationService
-      .executeJwtAuthenticationService(this.state.username, this.state.password)
-      .then((response) => {
-        console.log(response);
-        this.setState({
-          token: response.data.jwttoken
-        });
-        AuthenticationService.registerSuccessfulLoginForJwt(this.state.username, this.state.token)
-        this.props.history.push('/travel');
-      }).catch(() => {
-        this.setState({
-          showSuccessMessage: false,
-          hasLoginFailed: true
-        })
-      });
-  }
-
-  render() {
-    return (
-      <div id="Login">
-        <div id="login-section">
-          <div id="login-image">
-            <img src={LoginImage}></img>
-            <div id="login-description">
-              <h6>Travler</h6>
-              <h2>Keep it special</h2>
-              <p>Capture your personal memories <br></br>in a unique way.anywhere</p>
-            </div>
-          </div>
-          <div id="login-info">
-            <div id="login-title">
-              <h2>Traveler</h2>
-              <p>use your email account</p>
-            </div>
-            <div id="login-account">
-              <div id="login-email">
-                <input type="text" className="form-control" placeholder="Email" name="username" onChange={this.inputHandler}></input>
-              </div>
-              <div id="login-password">
-                <input type="password" className="form-control" placeholder="Password" name="password" onChange={this.inputHandler}>
-                </input>
-              </div>
-              <div id="login-forgot">
-                <Link to="/forgot">
-                  <p>Forgot your Password?</p>
-                </Link>
-              </div>
-              <div id="login-button">
-                <button className="btn btn-info" id="btnLogin" onClick={this.loginHandler}>SIGN IN</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+export default function Login() {
+  return (
+    <div>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main'}}>
+            {/* <LockOutlinedIcon /> */}
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <TextField 
+            margin="normal"
+            label="Email Address" 
+            required 
+            fullWidth 
+            name="email"
+            autoComplete='email'
+            autoFocus 
+          />
+          <TextField 
+            margin="normal"
+            label="Pssword" 
+            type="password" 
+            required 
+            fullWidth 
+            name="password"
+            autoComplete='current-password'
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" 
+            color="primary" />}
+            label="Remember me"
+          />
+          <Button type="submit" fullWidth variant="contained"
+          sx={{ mt: 3, mb: 2 }}>
+            Sign in
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link>Forgot password?</Link> 
+            </Grid>
+            <Grid item>
+              <Link>Sign up</Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  )
 }
